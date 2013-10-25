bad77ac4c06fed3047a11bcd4b5245777a64ab26
Chinmay Kulkarni <chinmay@stanford.edu>
2013-10-25 10:44:45 +0530

    changed database for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,154 +1,51 @@
-# Use RVM (ruby-version is on server)
-require "rubygems"
-require "bundler/capistrano"
-# set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-require "delayed/recipes"
+set :application, 'diode'
+set :repo_url, 'git@github.com:StanfordHCI/diode.git'
 
-# set :rails_env, "production" #added for delayed job  
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# Precompile Assets
-load 'deploy/assets'
+set :deploy_to, '/srv/www/diode/application'
+# set :scm, :git
+set :scm, :git
+set :branch, "master"
+# set :deploy_via, :export
+set :deploy_via, :remote_cache
+# set :pty, true
+# default_run_options[:pty] = true
+ # Must be set for the password prompt
 
-set :applicationdir, "/srv/www/diode/application"
-
-set :application, "Diode"
-set :scm, 'git'
-set :repository,  "git@github.com:StanfordHCI/diode.git"
-set :git_enable_submodules, 1 # if you have vendored rails
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-#Set forward agent because the server has multiple repositories cloned on it.
-set :ssh_options, { :forward_agent => true }
+# set :ssh_options, { :forward_agent => true }
 
 
+# set :format, :pretty
+set :log_level, :debug
 
-# desc "Run on ec2: Server in us-west-2b (Oregon)"
-# task :grader do
-#   set :rvm_ruby_string, 'ruby-1.9.3'
-#   require "rvm/capistrano"
-#   server "ubuntu@54.203.252.80", :web, :app, :db, :primary => true
-# end
 
-# desc "Run on ANOTHERSERVER"
-# task :clr3 do
-#   set :rvm_ruby_string, 'ruby-1.9.3'
-#   set :rvm_type, :system
-#   require "rvm/capistrano"
-#   server "root@ANOTHERSERVER", :web, :app, :db, :primary => true
-# end
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :deploy_to, applicationdir
-set :deploy_via, :export
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
 
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa"), File.join(ENV["HOME"], ".ssh","ec2.pem")]
-
-# Generate an additional task to fire up the thin clusters
 namespace :deploy do
-  desc "Start the Thin processes"
-  task :start do
-    run  <<-CMD
-      cd /srv/www/diode/application/current; bundle exec thin start -C config/thin.yml 
-    CMD
-  end
 
-  desc "Stop the Thin processes"
-  task :stop do
-    run <<-CMD
-      cd /srv/www/diode/application/current; bundle exec thin stop -C config/thin.yml 
-    CMD
-  end
-
-  desc "Restart the Thin processes"
+  desc 'Restart application'
   task :restart do
-    run <<-CMD
-      cd /srv/www/diode/application/current; bundle exec thin restart -C config/thin.yml
-    CMD
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      
+    end
   end
-  # namespace :assets do
-  #   desc "Precompile assets locally and then rsync to app servers"
-  #   task :precompile, :only => { :primary => true } do
-  #     run_locally "rake assets:precompile;"
-  #     servers = find_servers :roles => [:app], :except => { :no_release => true }
-  #     servers.each do |server|
-  #       run_locally "rsync -av ./public/assets/ #{user}@#{server}:#{current_path}/public/assets/;"
-  #     end
-  #     run_locally "rm -rf public/assets"
-  #   end
-  # end
 
-  # namespace :assets do
-  #       task :precompile, :roles => :web, :except => { :no_release => true } do
-  #         from = source.next_revision(current_revision)
-  #         if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-  #           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-  #         else
-  #           logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #         end
-  #     end
-  # end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
 end
-
-
-
-# Symlink database.yml on the server
-namespace :deploy do
-  desc "Symlinks the database.yml"
-  task :symlink_db, :roles => :app do
-    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
-  end
-
-  desc "Symlinks the constants file"
-  task :symlink_constants, :roles => :app do
-    run "ln -nfs #{deploy_to}/shared/config/initializers/constants.rb #{release_path}/config/initializers/constants.rb"
-  end
-
-  desc "Symlinks the log file"
-  task :symlink_log, :roles => :app do
-    run "ln -nfs #{deploy_to}/shared/log/production.log #{release_path}/log/production.log"
-  end
-
-  # desc "Symlinks the paperclip uploads folder"
-  # task :symlink_paperclip, :roles => :app do
-  #   run "ln -nfs #{deploy_to}/shared/log/production.log #{release_path}/log/production.log"
-  # end
-end
-
-
-
-# namespace :config do
-#   task :nginx, roles: :app do
-#     puts "Symlinking your nginx configuration..."
-#     sudo "ln -nfs #{release_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-#   end
-# end
-
-# after "deploy:setup", "config:nginx"
-
-
-# # Restart the delayed_job process
-# namespace :delayed_job do 
-#   desc "Restart the delayed_job process"
-#   task :restart, :roles => :app do
-#     run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
-#   end
-# end
-
-# Define all the tasks that need to be running manually after Capistrano is finished.
-before 'deploy:assets:symlink', 'deploy:symlink_constants'
-before 'deploy:assets:symlink', 'deploy:symlink_log'
-before 'deploy:assets:precompile', 'deploy:symlink_db'
-before 'deploy:restart', 'deploy:migrate'
-# before 'deploy:restart', 'delayed_job:restart' # deploy:update_code
-
-after "deploy:stop",    "delayed_job:stop"
-after "deploy:start",   "delayed_job:start"
-after "deploy:restart", "delayed_job:restart"
-
-# If you want to use command line options, for example to start multiple workers,
-# define a Capistrano variable delayed_job_args:
-#
-#   set :delayed_job_args, "-n 2"
-# require './config/boot'
-# require 'airbrake/capistrano'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,16 @@
 set :stage, :production
 
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{ubuntu@54.203.252.80}
+# role :all, %w{ubuntu@54.203.252.80}
+
 role :web, %w{ubuntu@54.203.252.80}
 role :db,  %w{ubuntu@54.203.252.80}
+role :app,  %w{ubuntu@54.203.252.80}
 
 # Extended Server Syntax
 # ======================
@@ -21,11 +24,12 @@ role :db,  %w{ubuntu@54.203.252.80}
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+ 	user: 'ubuntu',
+   keys: [File.join(ENV["HOME"], ".ssh", "id_rsa"), File.join(ENV["HOME"], ".ssh","DiscourseKey.pem"), File.join(ENV["HOME"], ".ssh","github_rsa")],
+   # forward_agent: true,
+   auth_methods: %w(publickey password)
+ }
 # and/or per server
 # server 'example.com',
 #   user: 'user_name',
