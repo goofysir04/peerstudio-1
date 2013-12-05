1e8ddb20c59fa0affe63f8ef6c9eed0485403f85
Chinmay Kulkarni <chinmay@stanford.edu>
2013-12-05 11:45:51 -0800

    get only first answer

--- app/controllers/grading_controller.rb
+++ app/controllers/grading_controller.rb
@@ -270,7 +270,7 @@ class GradingController < ApplicationController
 
   def staff_grade
     @question = Question.find(params[:id])
-    @answer = Answer.where("question_id = ? and current_score is NULL and id not in (select answer_id from evaluations where score is not null)", @question) #TODO fix actual user
+    @answer = Answer.where("question_id = ? and current_score is NULL and id not in (select answer_id from evaluations where score is not null)", @question).first #TODO fix actual user
 
     if @answer.nil?
       flash[:notice] = "We have no more answers for you to evaluate"
