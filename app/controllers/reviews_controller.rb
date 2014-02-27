b4ed105e9fd48dd3072e1352b67622d07fb5a87b
Chinmay Kulkarni <chinmay@stanford.edu>
2014-02-26 20:04:37 -0800

    allow reviews to be deleted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -80,9 +80,10 @@ class ReviewsController < ApplicationController
   # DELETE /reviews/1
   # DELETE /reviews/1.json
   def destroy
+    @assignment = @review.assignment
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url }
+      format.html { redirect_to @assignment }
       format.json { head :no_content }
     end
   end
