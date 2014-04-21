3f31ac142046677e53271f70d92cae1cd01a937a
Chinmay Kulkarni <chinmay@stanford.edu>
2014-04-21 10:17:35 -0700

    Validate presence of start and end date for rubric items

--- app/models/rubric_item.rb
+++ app/models/rubric_item.rb
@@ -5,4 +5,7 @@ class RubricItem < ActiveRecord::Base
   accepts_nested_attributes_for :answer_attributes, :allow_destroy => true, reject_if: proc { |attributes| attributes['description'].blank? }
 
   default_scope :order=>'ends_at ASC, open_at ASC, created_at ASC'
+
+  validates :ends_at, presence: true
+  validates :open_at, presence: true
 end
