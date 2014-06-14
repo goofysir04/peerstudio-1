9332ab1de5f6f2639c3578e715c6efed3911815f
Chinmay Kulkarni <chinmay@stanford.edu>
2014-06-14 13:12:57 -0700

    added a modal log in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,17 @@ module ApplicationHelper
 	def required_baseline
 		return 4
 	end
+
+	#default devise resources
+	def resource_name
+		:user
+	end
+
+	def resource
+		@resource ||= User.new
+	end
+
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
 end
