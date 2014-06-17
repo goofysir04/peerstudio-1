e138ed94159e7e46a59ef946ba4d575600a6b377
Chinmay Kulkarni <chinmay@stanford.edu>
2014-06-17 11:30:16 -0700

    users are not admins by default

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140615230843) do
+ActiveRecord::Schema.define(version: 20140617182452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -393,13 +393,13 @@ ActiveRecord::Schema.define(version: 20140615230843) do
     t.string   "provider"
     t.string   "uid"
     t.string   "gender"
-    t.boolean  "admin"
     t.integer  "cid"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.boolean  "consented"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
