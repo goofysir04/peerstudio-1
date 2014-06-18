390cc833fdff2100224345aec0a669c9e91bee82
Chinmay Kulkarni <chinmay@stanford.edu>
2014-06-17 20:55:20 -0700

    get fonts over https; don't create new drafts each time

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -26,7 +26,7 @@ class AnswersController < ApplicationController
   def new
     @latest_answer = Answer.where(assignment: @assignment, user: current_user).order('updated_at desc').first
 
-    if !@latest_answer.nil? and false
+    if !@latest_answer.nil?
       if !@latest_answer.submitted?
         redirect_to edit_answer_path(@latest_answer), notice: "We took you to the draft you were already editing" and return
       else
