2a944ea169671fd8556424ab0cc129242e1aecd4
Chinmay Kulkarni <chinmay@stanford.edu>
2014-07-01 14:38:14 -0700

    Added production target for deploy script

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,12 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{ubuntu@beta.peerstudio.org}
+# role :web, %w{ubuntu@beta.peerstudio.org}
+# role :db,  %w{ubuntu@beta.peerstudio.org}
+
+set :branch, "master"
+set :rails_env, "production"
 
 # Extended Server Syntax
 # ======================
@@ -15,8 +18,9 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'www.peerstudio.org', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
+# set :deploy_to, 
 set :deploy_to, '/srv/www/peerstudio'
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -40,4 +44,14 @@ set :deploy_to, '/srv/www/peerstudio'
 #   }
 # setting per server overrides global ssh_options
 
-# fetch(:default_env).merge!(rails_env: :production)
+task :check_write_permissions do
+  on roles(:all) do |host|
+	if test("[ -w #{fetch(:deploy_to)} ]")
+	  info "#{fetch(:deploy_to)} is writable on #{host}"
+	else
+	  error "#{fetch(:deploy_to)} is not writable on #{host}"
+	end
+  end
+end
+
+# fetch(:default_env).merge!(rails_env: :staging)
