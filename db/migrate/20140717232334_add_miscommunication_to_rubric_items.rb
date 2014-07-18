981ae4f14a93d0b25b3406e040d9c2a00a80611f
Chinmay <chinmay007@gmail.com>
2014-07-18 07:35:32 -0700

    Rubric item doesn't need miscommunication field.

--- db/migrate/20140717232334_add_miscommunication_to_rubric_items.rb
+++ db/migrate/20140717232334_add_miscommunication_to_rubric_items.rb
@@ -1,5 +1,5 @@
 class AddMiscommunicationToRubricItems < ActiveRecord::Migration
-  def change
-    add_column :rubric_items, :miscommunication, :boolean, :default => false
-  end
+  # def change
+  #   add_column :rubric_items, :miscommunication, :boolean, :default => false
+  # end
 end
