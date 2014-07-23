6755b58e7dafaca2704e608b2eb8f2b79e6a18ad
Courtney Noh <courtneynoh@gmail.com>
2014-07-23 15:55:32 -0700

    Ordered assignments by date + altered buttons + added opacity to closed assignments + edited closest_open function to be nicer

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,25 +10,7 @@ class Course < ActiveRecord::Base
   end
 
   def self.closest_open(id)
-    open_assignments_list = []
-    assignments = Assignment.where("course_id = ?", id)
-    for a in assignments
-      if !a.ended?
-        open_assignments_list << a
-      end
-    end
-    if open_assignments_list.empty? 
-      return nil
-    else
-      closest = open_assignments_list[0].due_at - DateTime.current()
-      closest_class = open_assignments_list[0]
-      for a in open_assignments_list
-        if (a.due_at - DateTime.current()) <= closest
-          closest_class = a
-          closest = a.due_at - DateTime.current()
-        end
-      end
-      return closest_class
-    end
+  	courses = Course.find(id).assignments.order('due_at asc').select{|a| !a.ended?}
+  	return courses[0]
   end
 end
