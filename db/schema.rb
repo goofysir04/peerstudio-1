09090437d5dfedcb2e5c72d4fdb31e81244f90a4
Courtney Noh <courtneynoh@gmail.com>
2014-08-13 12:13:09 -0700

    commit before I merge everything necessary pt 2

--- db/schema.rb
+++ db/schema.rb
@@ -376,19 +376,19 @@ ActiveRecord::Schema.define(version: 20140812140151) do
   add_index "trigger_actions", ["user_id"], name: "index_trigger_actions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                    default: "",                           null: false
-    t.string   "encrypted_password",       default: "",                           null: false
+    t.string   "email",                  default: "",                           null: false
+    t.string   "encrypted_password",     default: "",                           null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",            default: 0,                            null: false
+    t.integer  "sign_in_count",          default: 0,                            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "time_zone",                default: "Pacific Time (US & Canada)"
+    t.string   "time_zone",              default: "Pacific Time (US & Canada)"
     t.text     "name"
     t.string   "provider"
     t.string   "uid"
