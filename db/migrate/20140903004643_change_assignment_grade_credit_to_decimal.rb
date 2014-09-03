8b48c9d585ca7190943b4c09fbc5f3fccd200c1f
Chinmay Kulkarni <chinmay@stanford.edu>
2014-09-02 17:54:26 -0700

    typo with fields

--- db/migrate/20140903004643_change_assignment_grade_credit_to_decimal.rb
+++ db/migrate/20140903004643_change_assignment_grade_credit_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangeAssignmentGradeCreditToDecimal < ActiveRecord::Migration
   def change
-  	change_column :assignment_grades, :credit, :decimal, precision: 4, scale: 3
+  	change_column :assignment_grades, :credit, :decimal, scale: 3, precision: 5
   end
 end
