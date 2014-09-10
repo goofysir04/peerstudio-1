b51b1f6b36467dd822812c0e717ef409222dca53
Chinmay Kulkarni <chinmay@stanford.edu>
2014-09-10 14:06:20 -0700

    Changed reads to auth reads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -8,7 +8,7 @@ class UploadsController < ApplicationController
 	name_string = SecureRandom.uuid + File.extname(name)
 	
 	new_object = s3.buckets[ENV['S3_BUCKET_NAME']].objects[name_string]
-	return_url = (new_object.url_for(:put, secure: true, acl:'public_read', content_type: mimetype)).to_s
+	return_url = (new_object.url_for(:put, secure: true, acl:'authenticated_read', content_type: mimetype)).to_s
 
   	respond_to do |format|
   		format.js {render text: URI.encode(return_url)}
