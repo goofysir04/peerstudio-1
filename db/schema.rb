e44fd0624f2a6af5140baa918d54cb86b08912e7
Chinmay Kulkarni <chinmay@stanford.edu>
2014-10-08 23:14:41 -1000

    Use session store in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912204623) do
+ActiveRecord::Schema.define(version: 20141009091133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -388,6 +388,16 @@ ActiveRecord::Schema.define(version: 20140912204623) do
   add_index "rubric_items", ["assignment_id"], name: "index_rubric_items_on_assignment_id", using: :btree
   add_index "rubric_items", ["user_id"], name: "fk__rubric_items_user_id", using: :btree
   add_index "rubric_items", ["user_id"], name: "index_rubric_items_on_user_id", using: :btree
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
