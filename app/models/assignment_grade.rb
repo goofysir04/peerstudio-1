8f320d9c29da388c82b2e3289091fa5204dca4b5
Chinmay Kulkarni <chinmay@stanford.edu>
2014-11-28 15:59:46 -0800

    assignment grades with due dates

--- app/models/assignment_grade.rb
+++ app/models/assignment_grade.rb
@@ -5,11 +5,12 @@ class AssignmentGrade < ActiveRecord::Base
   belongs_to :answer
   def self.export_to_csv()
   	CSV.generate({}) do |csv|
-      csv << ['assignment','name', 'email', 'grade']
-      self.where(is_final: true, experimental: false).group(:assignment_id, :user_id).sum(:credit).each do |group,grade|
+      csv << ['assignment', 'name', 'email', 'grade', 'due_at','submitted_at','late']
+      self.where(is_final: true, experimental: false).group(:assignment_id, :user_id, :answer_id).sum(:credit).each do |group,grade|
         assignment = Assignment.find(group[0])
       	student = User.find(group[1])
-        csv << [assignment.title, student.name,student.email, grade.round(2)]
+        answer = Answer.find(group[2])
+        csv << [assignment.title, student.name,student.email, grade.round(2), assignment.due_at.in_time_zone("Pacific Time (US & Canada)").strftime("%d/%m/%y %H:%M:%S"), answer.submitted_at.in_time_zone("Pacific Time (US & Canada)").strftime("%d/%m/%y %H:%M:%S"), answer.submitted_at > assignment.due_at ? "Late": ""]
       end
     end
   end
